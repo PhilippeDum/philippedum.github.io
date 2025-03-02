421d0b1dbe0906190b2ce69f1dd9231fad8a2909
PhilippeDum <phil.dum88@gmail.com>
2025-03-02 19:30:09 +0100

    [UPDATE] Portfolio new test pages & add background image

--- src/app/_default_layout.tsx
+++ src/app/_default_layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const quicksandSans = Quicksand({
-    variable: "--font-quicksand-sans",
-    subsets: ["latin"],
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -19,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${quicksandSans.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
