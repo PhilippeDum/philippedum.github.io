87d81a62085ad5398590d643d99d6f58926e35d3
PhilippeDum <phil.dum88@gmail.com>
2025-03-04 00:22:09 +0100

    [UPDATE] fix database connection and 1sts api routes

---
+++ src/app/api/projects/route.js
@@ -0,0 +1,12 @@
+﻿// /src/app/api/users/route.js
+import { query } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    try {
+        const result = await query('SELECT * FROM projects');
+        return NextResponse.json(result.rows);
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,112 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="home">
-    {/*<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">*/}
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-        <Image src="/game_developer_banner.png" alt="developer_banner" layout="responsive" width={100} height={100}/>
+    const [demo_reels, setDemo_reels] = useState([]);
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-        <ul>
-          <li>Home</li>
-          <li >About</li>
-          <li>Projects</li>
-          <li>Github</li>
-        </ul>
+    useEffect(() => {
 
-          <Link key="link_default" href="/#">
-              <p className="hidden md:block">Link to default page</p>
-          </Link>
+        const fetchData = async () => {
+            try {
+                const [demo_reelsResponse, projectsResponse] = await Promise.all([
+                    fetch('/api/demo_reels'),
+                    fetch('/api/projects'),
+                ]);
 
-        <div className="projects">
-          <h1>List of my projects</h1>
+                if (!demo_reelsResponse.ok) {
+                    throw new Error('Erreur lors de la récupération des demo reels');
+                }
+
+                if (!projectsResponse.ok) {
+                    throw new Error('Erreur lors de la récupération des projects');
+                }
+
+                const demo_reelsData = await demo_reelsResponse.json();
+                const projectsData = await projectsResponse.json();
+
+                setDemo_reels(demo_reelsData);
+                setProjects(projectsData);
+            } catch (error) {
+                console.error("Erreur au fetch : ", error, " (", error.message, ")");
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) return <p>Chargement...</p>
+    if (error) return <p>Erreur: {error}</p>;
+
+    return (
+        <div className="home">
+        {/*<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">*/}
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+            <Image src="/game_developer_banner.png" alt="developer_banner" layout="responsive" width={100} height={100}/>
+
+              <div className="demo_reels">
+                  <h2>Demo Reels</h2>
+                  <ul>
+                      Count : {demo_reels.length}
+                      {demo_reels.map((demo_reel) => (
+                          // <li key={demo_reel['id']}> -- {demo_reel['id']}. {demo_reel['title']} : <a href={demo_reel['link']}>Click here</a></li>
+                          <li key={demo_reel['id']}> -- {demo_reel['id']}. {demo_reel['title']}:
+                              <Link key="link_default" href={demo_reel['link']} className="text-blue-500">
+                                  Click here
+                              </Link>
+                              <iframe
+                                  src={demo_reel['link']} title={demo_reel['title']}
+                                  className="w-full aspect-video self-stretch md:min-h-96"
+                                  frameBorder="0" aria-hidden="true"
+                                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                  referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
+                              />
+                          </li>
+                      ))}
+                  </ul>
+              </div>
+
+                <div className="projects">
+                    <h2>Projects</h2>
+                    <ul>
+                        Count : {projects.length}
+                        {projects.map((project) => (
+                            <li key={project['id']}> -- {project['id']}. {project['title']}</li>
+                      ))}
+                  </ul>
+              </div>
+
+              <div className="menu">
+                  <ul>
+                      <li>Home</li>
+                      <li >About</li>
+                      <li>Projects</li>
+                      <li>Github</li>
+                  </ul>
+              </div>
+
+              <Link key="link_default" href="/#">
+                  <p className="hidden md:block">Link to default page</p>
+              </Link>
+
+            </main>
+
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+                <h1>Footer</h1>
+                <p>This is the footer...</p>
+
+            </footer>
         </div>
-
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
-        <h1>Footer</h1>
-
-      </footer>
-    </div>
-  );
+    );
 }
