f9863c17f29e8bf04cb2b4f2f40cb046bfcafa39
PhilippeDum <phil.dum88@gmail.com>
2025-03-19 01:00:49 +0100

    [UPDATE] Simplify card parameter of a project

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,8 +1,8 @@
 ﻿// components/Card.js
 
-export default function Card({ title, tagline, date, images }) {
+export default function Card({ project }) {
 
-    const imagesList = images.split(',').map(item => item.trim());
+    const imagesList = project.images.split(',').map(item => item.trim());
     const firstImage = imagesList[0];
     const cardImage = "/projects/" + firstImage;
     const defaultImage = "/projects/a_venir.png";
@@ -16,11 +16,11 @@ export default function Card({ title, tagline, date, images }) {
                 onError={(e) => e.currentTarget.src = defaultImage} // default image
             />
             <div className="p-4 bg-gray-400 bg-opacity-30">
-                <h2 className="text-xl font-bold mb-2">{title}</h2>
-                <p className="text-gray-400">{tagline}</p>
+                <h2 className="text-xl font-bold mb-2">{project.title}</h2>
+                <p className="text-gray-400">{project.tagline}</p>
                 <div className="flex flex-row justify-between items-end">
                     <button className="mt-4 px-4 py-2 bg-blue-900 text-white rounded-lg hover:bg-blue-600 transition-colors">More...</button>
-                    <h3>{date}</h3>
+                    <h3>{project.date}</h3>
                 </div>
             </div>
         </div>
