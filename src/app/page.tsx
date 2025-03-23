03ca00c58783da565f6f10c43126646481366d89
PhilippeDum <phil.dum88@gmail.com>
2025-03-23 15:33:47 +0100

    [DELETE] Remove Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import {Navbar} from "@/app/components/Navbar";
 import {DemoReel, Project, Category} from "@/app/components/Data";
 import Card from "@/app/components/Card";
 
@@ -106,8 +105,6 @@ export default function Home() {
             <div className="relative z-10">
                 <Image src="/game_developer_banner.png" alt="developer_banner" layout="responsive" width={100} height={100}/>
                 <main className="m-5">
-
-                    <Navbar github_link={content["github_link"]} linkedin_link={content["linkedin_link"]} itchio_link={content["itchio_link"]} />
 
                     <br/><br/><br/><br/><br/><br/>
 
